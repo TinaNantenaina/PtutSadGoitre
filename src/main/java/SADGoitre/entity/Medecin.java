5f42ce39fa39d6729545b4355fb205d9f21c723b
TinaNantenaina <tina.nantenaina@etud.univ-jfc.fr>
2022-02-04 09:14:05 +0100

    création patient et médecin

--- src/main/java/SADGoitre/entity/Medecin.java
+++ src/main/java/SADGoitre/entity/Medecin.java
@@ -3,12 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.sadgoitre;
+package SADGoitre.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  *
- * @author tnantena
+ * @author Tina
  */
+@Entity
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    @NoArgsConstructor
+    @ToString
 public class Medecin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idMedecin;
     
+    @NonNull
+    private String nom;
+    
+    @NonNull
+    private String prenom;
+    
+    @NonNull
+    private String RPPS;
 }
