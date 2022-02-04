4ebb6eb0f07bbcce335b1666d01995c4c2fe2a2f
AgatheSk <ehtaga.kiratus@gmail.com>
2022-02-04 09:53:08 +0100

    modification de signe fonctionnel, (pas de mapping)

--- src/main/java/SADGoitre/entity/SigneFonctionnel.java
+++ src/main/java/SADGoitre/entity/SigneFonctionnel.java
@@ -5,10 +5,36 @@
  */
 package SADGoitre.entity;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  *
  * @author agath
  */
+@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Entity
 public class SigneFonctionnel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSigneFonctionnel;
+    
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private TypeSF typeSF;
+    
+    @NonNull
+    private List<String> nomSymptome;
     
 }
