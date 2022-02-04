cec2ec7f52bed148c593ea8dea6cbd978d92e172
AgatheSk <ehtaga.kiratus@gmail.com>
2022-02-04 09:12:55 +0100

    Ajout du debut de l'entité Anamnese

---
+++ src/main/java/SADGoitre/entity/Anamnese.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SADGoitre.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author agath
+ **/
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Anamnese {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idAnamnese;
+    
+    @NonNull
+    private boolean signeDeCompression;
+    
+    //Mapping
+}
