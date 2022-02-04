b16f1ce84d7117b802ace6f2da6598fca8bbb83a
AgatheSk <ehtaga.kiratus@gmail.com>
2022-02-04 08:42:19 +0100

    Ajout de l'entité Examen

--- src/main/java/SADGoitre/WebApp.java
+++ src/main/java/SADGoitre/WebApp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package SADGoitre;
+package SadGoitre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
