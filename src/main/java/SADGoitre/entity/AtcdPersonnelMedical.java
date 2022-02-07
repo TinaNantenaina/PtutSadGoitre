b545adb07f098e77482074e812ff97dfc6edbb8b
TinaNantenaina <tina.nantenaina@etud.univ-jfc.fr>
2022-02-07 16:33:17 +0100

    modification MedecinRepository

---
+++ src/main/java/SADGoitre/entity/AtcdPersonnelMedical.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SADGoitre.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Tina
+ */
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class AtcdPersonnelMedical {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idAtcdPers;
+     
+    @NonNull
+    private String nom;
+}
