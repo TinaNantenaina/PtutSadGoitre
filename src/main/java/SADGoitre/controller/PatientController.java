4bf374addbd3506602c645a6af7758c24811d5c4
AgatheSk <ehtaga.kiratus@gmail.com>
2022-02-07 17:57:35 +0100

    PatientController fini

--- src/main/java/SADGoitre/controller/PatientController.java
+++ src/main/java/SADGoitre/controller/PatientController.java
@@ -6,13 +6,18 @@
 package SADGoitre.controller;
 
 import SADGoitre.dao.MedecinRepository;
+import SADGoitre.dao.PatientRepository;
 import SADGoitre.entity.Medecin;
+import SADGoitre.entity.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -22,19 +27,73 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/patient")
 public class PatientController {
     @Autowired
+    private PatientRepository daoPatient;
+    
+    @Autowired
     private MedecinRepository daoMedecin;
     
     /**
      * Affiche toutes les patients du médecin
      *
      * @param model pour transmettre les informations à la vue
-     * @param user l'utilisateur connecté (un propriétaire)
-     * @return le nom de la vue à afficher ('afficheGTableaux.html')
+     * @param medecin le medecin connecté 
+     * @return le nom de la vue à afficher ('affichePatients.html')
      */
     @GetMapping(path = "show")
-    public String afficheTousLesPatients(Model model, @AuthenticationPrincipal Medecin medecin) {
+    public String afficheTousLesPatients(Model model, Medecin medecin) {
         model.addAttribute("patients", daoMedecin.getOne(medecin.getIdMedecin()));
         return "affichePatients";
     }
+    
+    /**
+     * Redirige vers la page d'un patient selon son id
+     *
+     * @param model pour transmettre les informations à la vue
+     * @param idPatient l'id du patient
+     * @return le nom de la vue à afficher ('detailPatient.html')
+     */
+    @GetMapping(path = "getPatient")
+    public String afficheUnPatient(Model model, Integer idPatient) {
+        model.addAttribute("patient", daoPatient.getOne(idPatient));
+        return "detailPatient";
+    }
+    
+    /**
+     * Montre le formulaire permettant d'ajouter un patient
+     *
+     * @param model pour transmettre les informations à la vue
+     * @param medecin Le medecin du patient
+     * @return le nom de la vue à afficher ('formulairePatient.html')
+     */
+    @GetMapping(path = "add")
+    public String montreLeFormulairePourAjout(Model model,Medecin medecin) {
+        model.addAttribute("patient", new Patient());
+        model.addAttribute("patients", daoMedecin.getOne(medecin.getIdMedecin()).getMesPatients());
+        return "formulairePatient";
+    }
+    
+    /**
+     * Appelé par 'formulairePatient.html', méthode POST
+     *
+     * @param patient le patient initialisé avec les valeurs saisies dans le formulaire (nom, prénom, genre et dtn)
+     * @param redirectInfo pour transmettre des paramètres lors de la redirection
+     * @return une redirection vers l'affichage de la liste des patients
+     */
+    @PostMapping(path = "save")
+    public String ajouteAnimalPuisMontreLaListe(Patient patient, RedirectAttributes redirectInfo) {
+        String message;
+        try {
+            daoPatient.save(patient);
+            // Le code de la catégorie a été initialisé par la BD au moment de l'insertion
+            message = "Le patient '" + patient.getNom() + patient.getPrenom() + "' a été correctement enregistrée";
+        } catch (DataIntegrityViolationException e) {
+            // Les noms sont définis comme 'UNIQUE' 
+            // En cas de doublon, JPA lève une exception de violation de contrainte d'intégrité
+            message = "Erreur : Le patient '" + patient.getNom() + patient.getPrenom() + "' existe déjà";
+        }
+        redirectInfo.addFlashAttribute("message", message);
+        return "redirect:show";
+    }
+    
     
 }
