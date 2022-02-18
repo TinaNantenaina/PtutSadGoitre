6fdb6c571fe31559bfae4b8c2d9c68e10d875abb
AgatheSk <ehtaga.kiratus@gmail.com>
2022-02-18 11:33:36 +0100

    Ajout d'un controleur pour l'accueil, plus de login de sécurité, changment du css de la page d'accueil --> PB d'affichage à régler

---
+++ src/main/java/SADGoitre/controller/AccueilController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SADGoitre.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author agath
+ */
+@Controller
+public class AccueilController {
+        @GetMapping({"/"})
+    public String welcome(Model model) {
+        return "PageAccueil";
+    }
+}
--- src/main/java/SADGoitre/controller/PatientController.java
+++ src/main/java/SADGoitre/controller/PatientController.java
@@ -11,7 +11,6 @@ import SADGoitre.entity.Medecin;
 import SADGoitre.entity.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
