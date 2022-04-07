36936589b0da46d02a581baa9c78345023bc4132
TinaNantenaina <tina.nantenaina@etud.univ-jfc.fr>
2022-04-07 18:50:06 +0200

    modification pour le drools

--- src/main/java/SADGoitre/WebApp.java
+++ src/main/java/SADGoitre/WebApp.java
@@ -5,9 +5,13 @@
  */
 package SadGoitre;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WebApp {
@@ -15,5 +19,10 @@ public class WebApp {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApp.class, args);
 	}
+        
+     @Bean
+     public static KieContainer kieContainer() {
+		return KieServices.Factory.get().getKieClasspathContainer();
+     }
 }
 
---
+++ src/main/java/SADGoitre/service/DroolAccessService.java
@@ -0,0 +1,28 @@
+
+package SADGoitre.service;
+
+import SADGoitre.dao.DiagnosticRepository;
+import SADGoitre.entity.Diagnostic;
+import SADGoitre.entity.Valeur_examen;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Tina
+ */
+@Service
+public class DroolAccessService {
+    @Autowired
+    private DiagnosticRepository daoDiagnostic;
+    
+    public List<Valeur_examen> recupererValeurExamen(int patientId) {
+        return null;
+        //retourner la liste des Valeur_examen  associÃ© Ã  un patient
+    }
+
+    public void insererDiagnostic (Diagnostic nouveauDiagnostic) {
+        //utiliser diagnostic respository pour inserer le nouveau diagnostic dans la base de donnÃ©es
+    }
+}
